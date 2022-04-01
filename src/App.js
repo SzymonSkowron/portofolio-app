9d159ceb3a35074032a49b6e1b690e18390e4668
Szymon Skowron <szymon.skowron1234@gmail.com>
2022-04-01 14:07:39 +0200

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import Home from "./components/Home";
+import Login from "./components/Login";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomeHeader from "./components/HomeHeader";
+import HomeThreeColumns from "./components/HomeThreeColumns";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <HomeHeader />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/Home' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/HomeThreeColumns' element={<HomeThreeColumns />}/>
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/components/HomeThreeColumns.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function HomeThreeColumns() {
+    return (
+        <div>
+           <h1>Home Three Columns</h1>
+        </div>
+    );
+}
+
+export default HomeThreeColumns;
