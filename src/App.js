aecc2c9f897c3d9707da058b372143e39781a33b
Szymon Skowron <szymon.skowron1234@gmail.com>
2022-04-05 20:31:56 +0200

    Change settings and first header look

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import './App.css'
-import Home from "./components/Home";
-import Login from "./components/Login";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomeHeader from "./components/HomeHeader";
-import HomeThreeColumns from "./components/HomeThreeColumns";
+
 
 function App() {
   return (
-      <Router>
-        <HomeHeader />
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/Home' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/HomeThreeColumns' element={<HomeThreeColumns />}/>
-        </Routes>
-      </Router>
+      <HomeHeader />
   );
 }
 
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,11 +1,44 @@
 import React from "react";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
+import img from "../assets/images/Home-Hero-Image.jpg"
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
 
 
 function HomeHeader() {
     return (
-        <div>
-            <h1>Home Header</h1>
-        </div>
+        <Router>
+            <div className="home__header__container">
+            <div className="home__background__img">
+                <img src={img} alt="box with things"/>
+            </div>
+                <div className="home__header">
+            <div className="home__header__log">
+                <ul>
+                    <li><Link to="/logowanie">Zaloguj</Link></li>
+                    <li><span><Link to="/rejestracja">Załóż konto</Link></span></li>
+                </ul>
+            </div>
+            <div className="home__header__nav">
+                <nav>
+                    <ul>
+                        <li><span><Link to="/">Start</Link></span></li>
+                        <li><Link to="/O co chodzi?">O co chodzi?</Link></li>
+                        <li><Link to="/O nas">O nas</Link></li>
+                        <li><Link to="/Fundacja i oragnizacje">Fundacja i organizacje</Link></li>
+                        <li><Link to="/Kontakt">Kontakt</Link></li>
+                    </ul>
+                </nav>
+            </div>
+                </div>
+            </div>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/logowanie' element={<Login />}/>
+                <Route path='/rejestracja' element={<Register />}/>
+            </Routes>
+        </Router>
     );
 }
 
